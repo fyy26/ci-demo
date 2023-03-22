8f6d041518e5e665ed79a9b87f0dc7f6d63f350e
Yuying Fan <yuyingfanus@gmail.com>
2023-03-22 18:09:17 -0400

    make application

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 #!/usr/local/bin/node
 
+const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
-console.log('Hello World: ' + uuidv4())
+console.log('Hello World: ' + uuidv4());
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World from Yuying');
+});
+
+app.listen(8000, () => {
+  console.log('Server running at http://localhost:8000/');
+});
+
+module.exports = app;
